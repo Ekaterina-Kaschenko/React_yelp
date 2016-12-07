8f11344b5ac5a3d084aa1bcfce7b9b21b6b6500e
Ekaterina Kashchenko <katikate123@gmail.com>
2016-12-07 19:06:25 +0200

    finished Clicking on Markers block

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -35,16 +35,22 @@ export class Container extends React.Component {
       })
   }
 
+  onMarkerClick(item) {
+    const {place} = item;
+    const {push} = this.context.router;
+    push(`/map/detail/${place.place_id}`)
+  }
+
   render() {
     let children = null;
     if (this.props.children) {
-      // We have children in the Container component
       children = React.cloneElement(
         this.props.children,
         {
           google: this.props.google,
           places: this.state.places,
-          loaded: this.props.loaded
+          loaded: this.props.loaded,
+          onMarkerClick: this.onMarkerClick.bind(this)
         });
     }
     return (
@@ -64,6 +70,10 @@ export class Container extends React.Component {
       </Map>
     )
   }
+}
+
+Container.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default GoogleApiWrapper({
--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -5,12 +5,26 @@ import Map, { Marker } from 'google-maps-react'
 import styles from './styles.module.css';
 
 export class MapComponent extends React.Component {
+  renderChildren() {
+    const {children} = this.props;
+    if (React.Children.count(children) > 0) {
+      return React.Children.map(children, c => {
+        return React.cloneElement(c, this.props, {
+          map: this.props.map,
+          google: this.props.google
+        })
+      })
+    } else {
+      return this._renderMarkers();
+    }
+  }
   renderMarkers() {
     if (!this.props.places) { return; }
-    return this.props.places.map(place =>{
+    return this.props.places.map(place => {
       return <Marker key={place.id}
                   name={place.id}
                   place={place}
+                  onClick={this.props.onMarkerClick.bind(this)}
                   label={p.name}
                   map={this.props.map}
                   position={place.geometry.location}
@@ -21,8 +35,10 @@ export class MapComponent extends React.Component {
     return (
       <Map google={this.props.google}
           map={this.props.map}
-          className={styles.map}>
-        {this.renderMarkers()}
+          className={styles.map}
+          visible={!children || React.Children.count(children) == 0}
+        >
+        {this.renderChildren()}
       </Map>
     )
   }
