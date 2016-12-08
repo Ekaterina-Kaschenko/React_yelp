3ef50566ff41770c8b766f6e929da2e02b2ba010
Ekaterina Kashchenko <katikate123@gmail.com>
2016-12-08 15:05:09 +0200

    try to display place's fotos instead map

--- src/views/Main/Detail/Detail.js
+++ src/views/Main/Detail/Detail.js
@@ -47,6 +47,17 @@ export class Detail extends React.Component {
     });
   }
 
+  renderPhotos(place) {
+    if (!place.photos || place.photos.length == 0) return;
+    const cfg = {maxWidth: 100, maxHeight: 100}
+    return (<div className={styles.photoStrip}>
+      {place.photos.map(p => {
+        const url = `${p.getUrl(cfg)}.png`
+        return (<img key={url} src={url} />)
+       })}
+     </div>)
+  }
+
   render() {
     if (this.state.loading) {
       return (<div className={styles.wrapper})>
@@ -59,6 +70,9 @@ export class Detail extends React.Component {
       <div className={styles.wrapper}>
         <div className={styles.header}>
           <h2>{place.name}</h2>
+        </div>
+        <div className={styles.details}>
+          {this.renderPhotos(place)}
         </div>
       </div>
     )
